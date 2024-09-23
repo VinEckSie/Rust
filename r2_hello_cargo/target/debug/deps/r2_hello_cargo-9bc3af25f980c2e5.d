61d9f0c0e3006462f3c0d578dbb1e46ab6c0c81b
Vincent <159125909+VinEckSie@users.noreply.github.com>
2024-09-23 14:24:51 +0000

    test

---
+++ r2_hello_cargo/target/debug/deps/r2_hello_cargo-9bc3af25f980c2e5.d
@@ -0,0 +1,5 @@
+/workspaces/Rust/r2_hello_cargo/target/debug/deps/libr2_hello_cargo-9bc3af25f980c2e5.rmeta: src/main.rs
+
+/workspaces/Rust/r2_hello_cargo/target/debug/deps/r2_hello_cargo-9bc3af25f980c2e5.d: src/main.rs
+
+src/main.rs:
---
+++ r2_hello_cargo/target/debug/deps/r2_hello_cargo-abd665e608114ec0.d
@@ -0,0 +1,5 @@
+/workspaces/Rust/r2_hello_cargo/target/debug/deps/libr2_hello_cargo-abd665e608114ec0.rmeta: src/main.rs
+
+/workspaces/Rust/r2_hello_cargo/target/debug/deps/r2_hello_cargo-abd665e608114ec0.d: src/main.rs
+
+src/main.rs:
